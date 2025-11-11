c395fdf9688f78c1480d64fc1690e4d90866c24d
elizabethharrison-akortiah <elizabethtemi1@gmail.com>
2025-11-11 23:49:01 +0000

    Started working on the viewCohort page

--- web/src/app/(dashboard)/cohorts/view/page.tsx
+++ web/src/app/(dashboard)/cohorts/view/page.tsx
@@ -1,10 +1,10 @@
 export default function ViewCohort() {
   return (
     <>
-      <p>
-        View a single cohort page with delete and edit action and cohort summary
-        information
-      </p>
+      <div className="p-6 sm:p-10 min-h-screen bg-gray-100 dark:bg-[#0b0c10] text-gray-900 dark:text-gray-100 transition-colors duration-300">
+
+      
+    </div>
     </>
   );
 }
