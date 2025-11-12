fe778adf6205591677959b12edb6cde4a3c36fe5
Sally <salomeytackie555@gmail.com>
2025-11-12 13:50:49 +0000

    created the edit page

--- web/src/app/(dashboard)/programs/add/page.tsx
+++ web/src/app/(dashboard)/programs/add/page.tsx
@@ -1,14 +1,22 @@
 import { Plus } from "lucide-react";
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
 
 export default function AddProgram() {
   return (
-    <>
+    <div className="bg-gray-100 rounded-lg p-6">
+    <div className="inline-block">
+      <Link href={'/programs'} className="flex  items-center gap-2 rounded-lg text-sm text-black  hover:bg-gray-300 px-3 py-2  transition-all duration-200 ">
+        <ArrowLeft size={18} className="transition-transform duration-200 group-hover:-translate-x-1" />
+        Back to programs
+      </Link>
+    </div>
       {/* <p>Add a new program page showing a form to fill and submit</p> */}
-      <div className="bg-white shadow-lg border">
-        <h1 className="text-black text-xl ml-6 mt-4 font-bold">Add New Program</h1>
+      <div className="mt-6 bg-white p-4 shadow-lg rounded-lg text-black">
+        <h1 className=" text-xl ml-6  font-bold">Add New Program</h1>
         <p className="mt-2 ml-6 text-gray-700 text-sm">Create a new cohort program by filling out the information below</p>
-        <div className="text-black w-[95%] mx-auto mt-6">
-          <h2>Basic Information</h2>
+        <div className=" w-[95%] mx-auto mt-6">
+          <h2 className="text-sm">Basic Information</h2>
           <form action="" className="space-y-6 mt-4">
             <div className=" flex flex-col justify-between">
               <label htmlFor="" className="text-sm text-gray-700 font-semibold">Program Name <span className="text-red-700">*</span></label>
@@ -25,7 +33,7 @@ export default function AddProgram() {
               </div>
               <div className=" flex flex-col justify-between">
                 <label htmlFor="" className=" block text-sm font-semibold text-gray-700 mb-1">Status <span className="text-red-700">*</span></label>
-                <select name="" id="" className="w-full  rounded-md px-2 py-1 bg-gray-50 focus:outline-none focus:ring-2 focus:ring-blue-500">
+                <select name="" id="" className="w-full text-sm text-gray-700 rounded-md px-2 py-1 bg-gray-50 focus:outline-none focus:ring-2 focus:ring-blue-500">
                   <option value="">upcoming</option>
                   <option value="">active</option>
                   <option value="">complete</option>
@@ -36,8 +44,8 @@ export default function AddProgram() {
           </form>
         </div>
         <div className="mt-10 border border-t-gray-400  "></div>
-        <div className="text-black mt-4 w-[95%] mx-auto" >
-          <h2 className="">Schedule & Participants</h2>
+        <div className=" mt-4 w-[95%] mx-auto" >
+          <h2 className="text-sm">Schedule & Participants</h2>
           <form action="" className="grid grid-cols-3 gap-4 mt-4 ">
             <div className="text-sm flex flex-col justify-between gap-2">
               <label htmlFor="">Start Date <span className="text-red-700">*</span></label>
@@ -55,10 +63,10 @@ export default function AddProgram() {
         </div>
          <div className="mt-10 border border-t-gray-400  "></div>
          <div className=" mt-4 w-[95%] mx-auto text-black ">
-          <h2 >Program Objectives</h2>
+          <h2 className="text-sm">Program Objectives</h2>
           <div className="mt-2 flex justify-between gap-4 ">
             <input type="text" placeholder="Add a program objective..." className="bg-gray-100 px-4 py-3 rounded-lg text-sm w-full focus:outline-none focus:ring-2 focus:ring-blue-500"  />
-            <button className="flex gap-2 border px-3 py-1 rounded-lg items-center bg-white text-black cursor-pointer">
+            <button className="flex gap-2 border px-3 py-1 rounded-lg items-center bg-white  cursor-pointer">
               <span><Plus /></span>
               <span>Add</span> 
             </button>
@@ -66,19 +74,19 @@ export default function AddProgram() {
           <p className="text-sm text-gray-600 mt-4">Please Enter or Click Add to include an objective</p>
          </div>
           <div className="mt-10 border border-t-gray-400  "></div>
-          <div className="text-black w-[95%] mx-auto mt-4">
-            <h2>Image(Optional)</h2>
+          <div className="w-[95%] mx-auto mt-4">
+            <h2 className="text-sm">Image(Optional)</h2>
             <div>
               <input type="file"  placeholder=" https://example.com/image.jpg" className="bg-gray-100 px-4 py-3 rounded-lg text-sm w-full mt-2 focus:outline-none focus:ring-2 focus:ring-blue-500" />
             </div>
           </div>
           <div className="mt-10 border border-t-gray-400  "></div>
 
-          <div className="text-black flex  gap-6 w-[95%] mx-auto mt-6 mb-10">
+          <div className=" flex  gap-6 w-[95%] mx-auto mt-6 mb-10">
             <button className="bg-black text-white px-4 py-2 rounded-lg cursor-pointer">Create program</button>
             <div className="px-4 py-2 rounded-lg border border-gray-600 cursor-pointer">Cancel</div>
           </div>
       </div>
-    </>
+    </div>
   );
 }
